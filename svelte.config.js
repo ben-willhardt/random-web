33344f669d116d104af255d0b9f4a07a432359aa
Benedict Willhardt <benedict@willhardt.dev>
2022-04-06 21:53:56 +0200

    nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter({
 		pages: 'build',
-		assets: 'build',
-		fallback: null
+		assets: 'build'
 		})
 	}
 };
