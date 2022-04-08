a426d4c2cc5a062a6fe6282393f7de01ecb884c5
Benedict Willhardt <bennewill@web.de>
2022-04-08 17:43:52 +0200

    Updates

---
+++ internal/chunks/ui-utils-2f8494e8.js
@@ -0,0 +1,1 @@
+const e=new Event("ThemeChange"),s=()=>{const t=document.getElementById("random-icon");t.classList.add("rotate-360"),setTimeout(()=>{t.classList.add("notransition"),t.classList.remove("rotate-360"),t.offsetHeight,t.classList.remove("notransition")},301)};export{e as T,s as r};
